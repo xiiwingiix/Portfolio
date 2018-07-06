31928b7fa8cebc65338500a8d3d24292e0e9c1fb
xiiwingiix <40253284+xiiwingiix@users.noreply.github.com>
2018-07-06 13:50:02 +0900

    반응형으로 변경 중

--- zara/js/index.js
+++ zara/js/index.js
@@ -2,36 +2,74 @@
 //헤더
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-
-    if (scroll <= 1000) {
-    	$("header").css("background", "rgba(255,255,255,0)");
-    	$("header>ul>li>a").css("color", "#fff");
-    	$("header>a.logo>img:first-child").css("display", "block");
-    	$("header>a.logo>img:last-child").css("display", "none");
-    	$("header>a.bag>img:nth-child(1)").css("display", "block");
-    	$("header>a.bag>img:nth-child(2)").css("display", "none");
-    } else if (scroll <=2670) {
-    	$("header").css("background","rgba(255,255,255,1)");
-    	$("header>ul>li>a").css("color", "#000");
-    	$("header>a.logo>img:first-child").css("display", "none");
-    	$("header>a.logo>img:last-child").css("display", "block");
-    	    	$("header>a.bag>img:nth-child(1)").css("display", "none");
-    	$("header>a.bag>img:nth-child(2)").css("display", "block");
+    var windowWidth = $(window).width();
+    if(windowWidth > 1211){
+        if (scroll <= 1000) {
+            $("header").css("background", "rgba(255,255,255,0)");
+            $("header>ul>li>a").css("color", "#fff");
+            $("header>a.logo>img:first-child").css("display", "block");
+            $("header>a.logo>img:last-child").css("display", "none");
+            $("header>a.bag>img:nth-child(1)").css("display", "block");
+            $("header>a.bag>img:nth-child(2)").css("display", "none");
+        } else if (scroll <=2670) {
+            $("header").css("background","rgba(255,255,255,1)");
+            $("header>ul>li>a").css("color", "#000");
+            $("header>a.logo>img:first-child").css("display", "none");
+            $("header>a.logo>img:last-child").css("display", "block");
+            $("header>a.bag>img:nth-child(1)").css("display", "none");
+        $("header>a.bag>img:nth-child(2)").css("display", "block");
     }else if (scroll <=3700) {
-    	$("header").css("background", "rgba(255,255,255,0)");
-    	$("header>ul>li>a").css("color", "#fff");
-    	$("header>a.logo>img:first-child").css("display", "block");
-    	$("header>a.logo>img:last-child").css("display", "none");
-    	$("header>a.bag>img:nth-child(1)").css("display", "block");
-    	$("header>a.bag>img:nth-child(2)").css("display", "none");
+            $("header").css("background", "rgba(255,255,255,0)");
+            $("header>ul>li>a").css("color", "#fff");
+            $("header>a.logo>img:first-child").css("display", "block");
+            $("header>a.logo>img:last-child").css("display", "none");
+            $("header>a.bag>img:nth-child(1)").css("display", "block");
+            $("header>a.bag>img:nth-child(2)").css("display", "none");
     }else {
-    	$("header").css("background","rgba(255,255,255,1)");
-    	$("header>ul>li>a").css("color", "#000");
-    	$("header>a.logo>img:first-child").css("display", "none");
-    	$("header>a.logo>img:last-child").css("display", "block");
-    	    	$("header>a.bag>img:nth-child(1)").css("display", "none");
-    	$("header>a.bag>img:nth-child(2)").css("display", "block");
+            $("header").css("background","rgba(255,255,255,1)");
+            $("header>ul>li>a").css("color", "#000");
+            $("header>a.logo>img:first-child").css("display", "none");
+            $("header>a.logo>img:last-child").css("display", "block");
+            $("header>a.bag>img:nth-child(1)").css("display", "none");
+            $("header>a.bag>img:nth-child(2)").css("display", "block");
     }
+    }else{
+        if (scroll <= 1000) {
+        $("header").css("background", "rgba(255,255,255,0)");
+        $("header>ul>li>a").css("color", "#fff");
+        $("header>a.logo>img:first-child").css("display", "block");
+        $("header>a.logo>img:last-child").css("display", "none");
+        $("header>a.bag>img:nth-child(1)").css("display", "none");
+        $("header>a.bag>img:nth-child(2)").css("display", "none");
+        $("header>a.bag>p>i").css("color", "rgba(255,255,255,1)");
+    } else if (scroll <=2670) {
+        $("header").css("background","rgba(255,255,255,1)");
+        $("header>ul>li>a").css("color", "#000");
+        $("header>a.logo>img:first-child").css("display", "none");
+        $("header>a.logo>img:last-child").css("display", "block");
+        $("header>a.bag>img:nth-child(1)").css("display", "none");
+        $("header>a.bag>img:nth-child(2)").css("display", "none");
+        $("header>a.bag>p>i").css("color", "rgba(0,0,0,1)");
+    }else if (scroll <=3700) {
+        $("header").css("background", "rgba(255,255,255,0)");
+        $("header>ul>li>a").css("color", "#fff");
+        $("header>a.logo>img:first-child").css("display", "block");
+        $("header>a.logo>img:last-child").css("display", "none");
+        $("header>a.bag>img:nth-child(1)").css("display", "none");
+        $("header>a.bag>img:nth-child(2)").css("display", "none");
+        $("header>a.bag>p>i").css("color", "rgba(255,255,255,1)");
+    }else {
+        $("header").css("background","rgba(255,255,255,1)");
+        $("header>ul>li>a").css("color", "#000");
+        $("header>a.logo>img:first-child").css("display", "none");
+        $("header>a.logo>img:last-child").css("display", "block");
+        $("header>a.bag>img:nth-child(1)").css("display", "none");
+        $("header>a.bag>img:nth-child(2)").css("display", "none");
+        $("header>a.bag>p>i").css("color", "rgba(0,0,0,1)");
+    }
+
+    }
+    
 
 });
 
