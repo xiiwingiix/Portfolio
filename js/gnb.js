7eb98c311225f45727d42184c3c25e6e4612708f
Dreaming Publisher Sung! <40253284+xiiwingiix@users.noreply.github.com>
2019-10-20 22:30:00 +0900

    update

---
+++ js/gnb.js
@@ -0,0 +1,30 @@
+var ele = '';
+
+ele += ('<ul id="gnb">');
+ele += ('	<li>');
+ele += ('		<a href="/Portfolio/index.html" class="scrolling white" >');
+ele += ('			<span>index</span><span>첫 화면</span>');
+ele += ('		</a>');
+ele += ('	</li>');
+ele += ('	<li>');
+ele += ('		<a href="#sec2" class="white" >');
+ele += ('			<span>some Work</span><span>작업물</span>');
+ele += ('		</a>');
+ele += ('	</li>');
+ele += ('	<li>');
+ele += ('		<a href="#sec3" class="white">');
+ele += ('			<span>skills</span><span>보유 기술</span>');
+ele += ('		</a>');
+ele += ('	</li>');
+ele += ('	<li>');
+ele += ('		<a href="#sec4" >');
+ele += ('			<span>reach me</span><span>연락처</span>');
+ele += ('		</a>');
+ele += ('	</li>');
+ele += ('	<li>');
+ele += ('		<i class="fas fa-bars"></i>');
+ele += ('	</li>');
+ele += ('</ul>');
+
+
+document.write(ele);
