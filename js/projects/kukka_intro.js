f352989821efca21e820439892729bcc90e3d04f
Dreaming Publisher Sung! <40253284+xiiwingiix@users.noreply.github.com>
2020-06-14 19:04:17 +0900

    kukka_newproduct_gXHR
    new_product 가져오기

--- js/projects/kukka_intro.js
+++ js/projects/kukka_intro.js
@@ -27,6 +27,81 @@ function showSlides() {
 
 showSlides()
 
+function get_newProduct(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'https://run.mocky.io/v3/3f93c722-e270-4b8f-bf85-85f010d1ccf6');
+	
+	// xhr.responseType = 'json';
+
+	xhr.onload = function(){
+		var data = JSON.parse(xhr.response)
+		var new_product = data.new_product;
+		
+		var el ='';
+		new_product.forEach(function(item){
+			el += '<li>'
+			el += '	<img src="'+item.imgSrc+'" alt="'+item.alt+'">'
+			el += '	<p class="tit">'+item.tit+'</p>'
+			el += '	<p class="txt">'+item.txt+'</p>'
+			el += '	<p class="price">'+item.price+'</p>'
+			el += '</li>'
+		});
+
+		var group_slide = document.querySelector('.group_slide');
+		group_slide.innerHTML = el;
+	};
+	
+	xhr.send()
+}
+
+get_newProduct()
+
+
+// var json = {
+// 	"new_product":[{
+// 		"imgSrc":"../../../images/kukka/Sansevieria.png",
+// 		"alt" : "산세베리아",
+// 		"tit" : "Sansevieria",
+// 		"txt" : "공기정화식물",
+// 		"price" : "3000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/RubberPlant.png",
+// 		"alt" : "고무나무",
+// 		"tit" : "RUBBER Plant",
+// 		"txt" : "공기정화식물",
+// 		"price" : "4000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/goldPepe.png",
+// 		"alt" : "황금페페",
+// 		"tit" : "Pepe gold",
+// 		"txt" : "공기정화식물",
+// 		"price" : "3000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/HolyPepe.png",
+// 		"alt" : "홀리페페",
+// 		"tit" : "Holi Pepe",
+// 		"txt" : "공기정화식물",
+// 		"price" : "3000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/Goosefoot.png",
+// 		"alt" : "싱고니움",
+// 		"tit" : "Syngonium",
+// 		"txt" : "공기정화식물",
+// 		"price" : "4000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/Redpeperomia.png",
+// 		"alt" : "홍페페",
+// 		"tit" : "Red peperomia",
+// 		"txt" : "공기정화식물",
+// 		"price" : "3000"
+// 	},{
+// 		"imgSrc":"../../../images/kukka/stuki.png",
+// 		"alt" : "스투키",
+// 		"tit" : "stuki",
+// 		"txt" : "공기정화식물",
+// 		"price" : "3000"
+// 	}]
+// }
 
 //section3
 	var a = 1;
