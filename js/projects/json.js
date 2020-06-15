00e6911dc98e7ef7e08ea0b51a85e96aa2f18509
Dreaming Publisher Sung! <40253284+xiiwingiix@users.noreply.github.com>
2020-06-16 00:11:14 +0900

    kukka_intro_seasonpdt

--- js/projects/json.js
+++ js/projects/json.js
@@ -1,4 +1,4 @@
-var kukka_intro = {
+var kukka_intro_product = {
 	"new_product":[{
 		"imgSrc":"../../../images/kukka/Sansevieria.png",
 		"alt" : "산세베리아",
@@ -30,11 +30,11 @@ var kukka_intro = {
 		"txt" : "공기정화식물",
 		"price" : "4500"
 	},{
-			"imgSrc":"../../../images/kukka/banyantree.png",
-			"alt" : "싱고니움",
-			"tit" : "Syngonium",
-			"txt" : "공기정화식물",
-			"price" : "4000"
+		"imgSrc":"../../../images/kukka/banyantree.png",
+		"alt" : "싱고니움",
+		"tit" : "Syngonium",
+		"txt" : "공기정화식물",
+		"price" : "4000"
 		},{
 		"imgSrc":"../../../images/kukka/Redpeperomia.png",
 		"alt" : "홍페페",
@@ -47,5 +47,25 @@ var kukka_intro = {
 		"tit" : "stuki",
 		"txt" : "공기정화식물",
 		"price" : "3000"
+	}],
+	"season_pdt":
+	[{
+		"imgSrc" :"../../../images/kukka/sunflower.jpg",
+		"alt":"해바라기",
+		"tit":"여름의 시작에 꼭 어울리는 꽃다발",
+		"txt":"너만 바라보겠다는 사랑스러운 맹세, 해바라기",
+		"price":"11,900"
+	},{
+		"imgSrc" :"../../../images/kukka/rose.jpg",
+		"alt":"장미",
+		"tit":"이런 꽃다발이면, 오늘 하루가 설레겠죠? ",
+		"txt":"핑크로 물드는, 핑크하트장비 에디션",
+		"price":"11,900"
+	},{
+		"imgSrc" :"../../../images/kukka/153flower.jpg",
+		"alt":"모나미 콜라보레이션",
+		"tit":"모나비와 두번째 콜라보레이션",
+		"txt":"오랫동안 간직하는 드라이플라워와 감성 모나미 153 에디션",
+		"price":"11,900"
 	}]
 }
--- js/projects/kukka_intro.js
+++ js/projects/kukka_intro.js
@@ -29,37 +29,50 @@ showSlides();
 // slide_show - end 
 
 
-// get_new_product 
-var nProduct_length ;
-
-function get_newProduct(){
+// get_product 
+function get_Product(){
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://run.mocky.io/v3/3ce1e415-175a-48b7-a419-ca24bc401c3e');
+	xhr.open('GET', 'https://run.mocky.io/v3/b7994ce8-1b02-4891-a501-4f7548bdd006');
 	
 	// xhr.responseType = 'json';
 
 	xhr.onload = function(){
 		var data = JSON.parse(xhr.response)
 		var new_pdt = data.new_product;
+		var season_pdt = data.season_pdt;
 		
-		var el ='';
+		var new_el ='';
 		new_pdt.forEach(function(item){
-			el += '<li class="gSlide">'
-			el += '	<img src="'+item.imgSrc+'" alt="'+item.alt+'">'
-			el += '	<p class="tit mt10">'+item.tit+'</p>'
-			el += '	<p class="txt mt10">'+item.txt+'</p>'
-			el += '	<p class="price text_brown">\\'+numberFormat(item.price)+'</p>'
-			el += '</li>'
+			new_el += '<li class="gSlide">'
+			new_el += '	<img src="'+item.imgSrc+'" alt="'+item.alt+'">'
+			new_el += '	<p class="tit mt10">'+item.tit+'</p>'
+			new_el += '	<p class="txt mt10">'+item.txt+'</p>'
+			new_el += '	<p class="price text_brown">\\'+numberFormat(item.price)+'</p>'
+			new_el += '</li>'
 		});
 
 		var group_slide = document.querySelector('.group_slide');
-		group_slide.innerHTML = el;
+		group_slide.innerHTML = new_el;
+
+		var season_el = '';
+		season_pdt.forEach(function(item){
+			season_el += '<div>'
+			season_el += '	<img src="'+item.imgSrc+'" alt="'+item.alt+'">'
+			season_el += '	<div class="btn_buy">Buy now ></div>'
+			season_el += '	<h6>'+item.tit+'</h6>'
+			season_el += '	<p class="txt">'+item.txt+'</p>'
+			season_el += '	<p class="price">\\'+item.price+'</p>'
+			season_el += '</div>'
+		});
+
+		var season_list = document.querySelector('.season_pdt .pdt_list');
+		season_list.innerHTML = season_el;
 
 	};
 	
 	xhr.send()
 }
-get_newProduct()
+get_Product()
 // get_new_product : end
 
 // 신상품 슬라이드 이동 
@@ -83,3 +96,5 @@ function plusSlides(state) {
 var btn_more = document.querySelector('.btn_more');
 btn_more.addEventListener('click', function(){popAlert('더보기','상단 카테고리 중 <span class="bold">플랜트정기구독</span>을 클릭해주세요.')});
 // read_more 클릭 :end
+
+
